c5293445b41792ca4b533752cffb6f866698706f
jiansheng010 <teoh.15@osu.edu>
2019-04-20 09:52:44 -0400

    Test

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-load "config/recipes/faye.rb"
+load "config/recipes/faye"
 
 ## Defaults:
 # set :scm,           :git
